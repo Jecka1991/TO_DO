5a1c2fd9d7e3bc3ef8c728ad644f24a36514ae9b
Евгений Кулеша <tms-21-online/>
2021-09-16 15:58:41 +0300

    Added an app for lists with intentionally not working unit test

--- superlists/superlists/functional_tests.py
+++ superlists/superlists/functional_tests.py
@@ -9,33 +9,32 @@ class NewVisitorTest(unittest.TestCase):
 def setUp(self):
     'установка'
 
-
-self.browser = webdriver.Firefox()
+    self.browser = webdriver.Firefox()
 
 
 def tearDown(self):
     'демонтаж'
 
-
-self.browser.quit()
+    self.browser.quit()
 
 
 def test_can_start_a_list_and_retrieve_it_later(self):
     'тест: можно начать список и получить его позже'
 
+    # Эдит слышала про крутое новое онлайн-приложение со
+    # списком неотложных дел. Она решает оценить его
+    # домашнюю страницу
 
-# Эдит слышала про крутое новое онлайн-приложение со
-# списком неотложных дел. Она решает оценить его
-# домашнюю страницу
+    self.browser.get('http://localhost:8000')
 
-self.browser.get('http://localhost:8000')
+    # Она видит, что заголовок и шапка страницы говорят о
+    # списках неотложных дел
+    self.assertIn('To-Do', self.browser.title)
+    self.fail('Закончить тест!')
 
-# Она видит, что заголовок и шапка страницы говорят о
-# списках неотложных дел
-self.assertIn('To-Do', self.browser.title)
-self.fail('Закончить тест!')
 
 # Ей сразу же предлагается ввести элемент списка
 
+
 if __name__ == '__main__':
-unittest.main(warnings='ignore')
+    unittest.main(warnings='ignore')
